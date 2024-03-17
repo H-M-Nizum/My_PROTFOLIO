a217d89a8295aec85ace37c63852ada6fdeb0f2e
H-M-Nizum <hmnizum1714032@gmail.com>
2024-03-18 03:59:43 +0600

    My Protfolio

---
+++ app.js
@@ -0,0 +1,376 @@
+// --------------------------------------Project data--------------------------------------------
+const fullstack = [
+    {
+        id : 1,
+        name: ' Digital Medical Services',
+        type : 'Fullstack',
+        tecnology : 'Python, Django, Bootstrap5, CSS, Html5, JavaScripts, Database:SQLite',
+        feature : `● Admin manages doctors, medicines, medical test and employees.
+        ● Admin updates appointment statuses.
+        ● Users register with a valid email, log in/out, and schedule appointments.
+        ● Authenticated users can filter doctors and book virtual meetings.
+        ● Patients access medical services, order medicines, apply for tests, and contact authorities.
+        ● Doctors provide prescriptions, downloadable as PDFs.
+        `,
+        github : 'https://github.com/H-M-Nizum/LifeTech_Care1',
+        livelink : 'https://digital-medical-services.onrender.com/',
+        thumbnail : 'img/project/hospital.png',
+    },
+    {
+        id : 2,
+        name: 'Tuition Media Platform',
+        type : 'Fullstack',
+        tecnology : ' Python, Django, Bootstrap5, CSS, Html5, Database:SQLite.',
+        feature : `● Users authenticate by submitting information, user can access tuition details and Through reviews.
+        ● They apply for tuition; admin selects applicable applicants for a particular tuition.
+        ● When a user secures a tuition, the system adds this tuition to the user's profile and the user receives a
+        success email.        
+        `,
+        github : 'https://github.com/H-M-Nizum/Tution_Media',
+        livelink : 'https://lifetech-tution.onrender.com/',
+        thumbnail : 'img/project/tution.png',
+    },
+    {
+        id : 3,
+        name: ' Library Management System',
+        type : 'Fullstack',
+        tecnology : 'Python, Django, Bootstrap5, CSS, Html5, JavaScripts, Database:SQLite',
+        feature : `Django library management project using Authentication and Authorization system. 
+        User can see all book, Filter by categorize. user can borrowed book. 
+        After read in book user through feedback and finally user can return book. 
+        # user can signup, login, logout
+        # user view profile and can change login password
+        # See all book
+        # email verification
+        # Borrowed book
+        # Write comment
+        # Return book
+        `,
+        github : 'https://github.com/H-M-Nizum/library_management',
+        livelink : 'https://dj-library.onrender.com/',
+        thumbnail : 'img/project/library.png',
+    },
+    {
+        id : 4,
+        name: ' Attendance Application',
+        type : 'Fullstack',
+        tecnology : 'Python, Django, Bootstrap5, CSS, Html5, JavaScripts, Database:SQLite',
+        feature : `Admin
+                        1.Admin can add teacher and student
+                        2. Admin can every single work in this system
+                    Teacher
+                        1. Teacher can apply in the School. If admin approve the application. Then teacher can login this system.
+                        2. Teacher can see all student and take attendance
+                        3. Teacher can add student and also remove student.
+                    Student
+                        1. student can see all student in the school
+                        2. Sudent can her/his own attendance history.
+                `,
+        github : 'https://github.com/H-M-Nizum/Attendance_Application',
+        livelink : '#',
+        thumbnail : 'img/project/library.png',
+    },
+
+]
+
+const backend = [
+    {
+        id : 50,
+        name: 'Blood Bank Application',
+        type : 'Backend',
+        tecnology : 'Python, Django, Django Rest Framework, Database:SQLite',
+        feature : `Using django rest framework
+        `,
+        github : 'https://github.com/H-M-Nizum/bloodbank',
+        livelink : 'https://lifesafe-bank.onrender.com/',
+        thumbnail : 'img/project/b5.jpg',
+    },
+    {
+        id : 51,
+        name: ' Track Corporate Assets',
+        type : 'Back-end',
+        tecnology : 'Python, Django, Django Rest Framework, Database:SQLite',
+        feature : `This is a Django app to track corporate assets such as phones, tablets, laptops and other gears handed out to employees. The application might be used by several companies. Each company might add all or some of its employees. Each company and its staff might delegate one or more devices to employees for a certain period of time. Each company should be able to see when a Device was checked out and returned. Each device should have a log of what condition it was handed out and returned.
+        `,
+        github : 'https://github.com/H-M-Nizum/trackCorporateAssets',
+        livelink : 'https://track-corporate-assets.onrender.com/',
+        thumbnail : 'img/project/track.jpg',
+    },
+    {
+        id : 52,
+        name: ' Tuition Media Platform',
+        type : 'Backend',
+        tecnology : 'Python, Django, Django Rest Framework, Database:SQLite',
+        feature : `● Users authenticate by submitting information, user can access tuition details and Through reviews.
+        ● They apply for tuition; admin selects applicable applicants for a particular tuition.
+        ● When a user secures a tuition, the system adds this tuition to the user's profile and the user receives a
+        success email. 
+        Endpoint :-- tution/, user/, apply/, contact_us/ 
+        `,
+        github : 'https://github.com/H-M-Nizum/Tuition_Media_Platform',
+        livelink : 'https://tution-media.onrender.com/',
+        thumbnail : 'img/project/tution1.png',
+    },
+    {
+        id : 53,
+        name: ' Digital Medical services',
+        type : 'Back-end',
+        tecnology : 'Python, Django, Django Rest Framework, Database:SQLite',
+        feature : `Admin
+                    1. Admin can add doctor
+                    2. Admin can do over all work in this system
+                    3. Admin can change all appointment status
+                Doctor
+                    1. Doctor can signup and logoin with admin approval
+                    2. One to one Meet with patient
+                    3. Can change appointmet satus
+                Patient
+                    1. Patient can take appointment
+                    2. Meet with doctor
+                    3. Through review after take appointment complete
+                    4. And all review
+                Endpoint :-- patient/, doctor/, service/, appointment/, contact_us/ 
+                    `
+                ,
+        github : 'https://github.com/H-M-Nizum/LifeTech_Care',
+        livelink : 'https://lifetech-care.onrender.com/',
+        thumbnail : 'img/project/hospital2.webp',
+    },
+]
+
+const frontend = [
+    {
+        id : 100,
+        name: ' Blood Bank Management System',
+        type : 'Front_end',
+        tecnology : 'Bootstrap5, CSS, Html5, JavaScripts',
+        feature : `Using Javascript, Bootstrap, Css, Html
+        `,
+        github : 'https://github.com/H-M-Nizum/bloodbank_frontend',
+        livelink : 'https://h-m-nizum.github.io/bloodbank_frontend/',
+        thumbnail : 'img/project/blood.png',
+    },
+    {
+        id : 101,
+        name: ' Simple Youtube clone',
+        type : 'Front-end',
+        tecnology : 'Bootstrap5, CSS, Html5, JavaScripts',
+        feature : `
+            Youtube clone
+        `,
+        github : 'https://github.com/H-M-Nizum/MY_TUBE',
+        livelink : 'https://h-m-nizum.github.io/MY_TUBE/',
+        thumbnail : 'img/project/youtube1.png',
+    },
+    {
+        id : 102,
+        name: ' E_comarce Website',
+        type : 'Front-end',
+        tecnology : 'Bootstrap5, CSS, Html5, JavaScripts',
+        feature : `This is a static project. Using only html , css and Bootstrap 5. It's a simple e-commerce website.
+            1. Home
+            2. Shoes
+            3. Backpack
+            4. Subscriber
+        `,
+        github : 'https://github.com/H-M-Nizum/E_comarce_bootstrap',
+        livelink : 'https://h-m-nizum.github.io/E_comarce_bootstrap/',
+        thumbnail : 'img/project/e_comars.png',
+    },
+    {
+        id : 103,
+        name: ' Hospital Management System',
+        type : 'Front-end',
+        tecnology : ' Bootstrap5, CSS, Html5, JavaScripts',
+        feature : `This is very simple digital Medical services project. Using only html and css.
+
+                
+                1. Home
+                2. About
+                3. Services
+                4. Contact
+        `,
+        github : 'https://github.com/H-M-Nizum/Hospital',
+        livelink : 'https://h-m-nizum.github.io/Hospital/',
+        thumbnail : 'img/project/hospital1.png',
+    },
+    {
+        id : 104,
+        name: ' Phone Stor',
+        type : 'Front-end',
+        tecnology : ' Bootstrap5, CSS, Html5, JavaScripts',
+        feature : `This is very simple Phone store services project. Using only html , css and Bootstrap 5. Only static frontend.
+
+                
+                1. Home
+                2. About
+                3. Services
+                4. Contact
+        `,
+        github : 'https://github.com/H-M-Nizum/Phone_Store',
+        livelink : 'https://h-m-nizum.github.io/Phone_Store/',
+        thumbnail : 'img/project/phone.png',
+    },
+    {
+        id : 105,
+        name: ' Text Editor',
+        type : 'Front-end',
+        tecnology : ' Bootstrap5, CSS, Html5, JavaScripts',
+        feature : `Text editor project. user write something here. Edit text like (Bold, underline, align text)
+
+        
+        1. User can write in editor
+        2. can change syle like(underline, bold etc)
+        3. user can setup text align.
+        `,
+        github : 'https://github.com/H-M-Nizum/Text_Editor',
+        livelink : 'https://h-m-nizum.github.io/Text_Editor/',
+        thumbnail : 'img/project/text.png',
+    },
+]
+
+// -----------------------------------------------------------------------------------------------
+// -----------------------------------         Function        -----------------------------------
+// -----------------------------------------------------------------------------------------------
+
+
+const loadproject = (parent_body, array) => {
+    const parentdiv = document.getElementById(parent_body)
+    for(let i=0; i<3; i++){
+        const newdiv = document.createElement("div")
+        newdiv.classList.add("col")
+        const data = array[i]
+        // console.log(data)
+        newdiv.innerHTML = `
+        <div class="card" style="width: 19rem;">
+            <img src="${data.thumbnail}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <p class="card-text">${data.type} Project</p>
+                <button href="#" onclick=showmodal(${data.id}) class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Details</button>
+            </div>
+        </div>
+        `
+        parentdiv.appendChild(newdiv)
+    }
+}
+
+loadproject("back_body", backend)
+loadproject("project_body", fullstack)
+loadproject("front_body", frontend)
+
+const seemore = (parent_body, more, less, array ) => {
+    console.log(parent_body, more, less, array)
+    const parentdiv = document.getElementById(parent_body)
+    document.getElementById(more).classList.add("d-none")
+    document.getElementById(less).classList.remove("d-none")
+    for(let i=3; i<array.length; i++){
+        const newdiv = document.createElement("div")
+        newdiv.classList.add("col")
+        const data = array[i]
+        // console.log(data)
+        newdiv.innerHTML = `
+        <div class="card" style="width: 19rem;">
+            <img src="${data.thumbnail}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.name}</h5>
+                <p class="card-text">${data.type} Project</p>
+                <a href="#" class="btn btn-primary">Details</a>
+            </div>
+        </div>
+        `
+        parentdiv.appendChild(newdiv)
+    }
+}
+
+document.getElementById("fullstackmore").addEventListener('click', function(){
+    seemore("project_body", "fullstackmore", "fullstackless", fullstack)
+})
+document.getElementById("backmore").addEventListener('click', function(){
+    seemore("back_body", "backmore", "backless", backend)
+})
+document.getElementById("frontmore").addEventListener('click', function(){
+    seemore("front_body", "frontmore", "frontless", frontend)
+})
+
+
+
+const seeless = (body, more, less, array) => {
+    document.getElementById(more).classList.remove("d-none")
+    document.getElementById(less).classList.add("d-none")
+    document.getElementById(body).innerHTML = ""
+    loadproject(body, array)
+}
+
+document.getElementById("fullstackmore").addEventListener('click', function(){
+    seeless("project_body", "fullstackmore", "fullstackless", fullstack)
+})
+document.getElementById("backless").addEventListener('click', function(){
+    seeless("back_body", "backmore", "backless", backend)
+})
+document.getElementById("frontless").addEventListener('click', function(){
+    seeless("front_body", "frontmore", "frontless", frontend)
+})
+
+
+// --------------------------------------------- Modal -------------------------------------------
+
+// function for modal
+const modalbody = (data) => {
+    document.getElementById("exampleModalLabel").innerText = data.name
+    const parentdiv = document.getElementById("modal_body")
+    parentdiv.innerHTML = `
+    <img style="width: 19rem;" src="${data.thumbnail}" alt="">
+    <div>
+        <h3>${data.name} || ${data.type}</h3>
+        <p><b style="font-size: 20px;">Feature : </b> ${data.feature}</p>
+        <p><b style="font-size: 20px;"> Tecnology : </b> ${data.tecnology}</p>
+        <p><b style="font-size: 20px;"> Github : </b> ${data.github}</p>
+        <p><b style="font-size: 20px;"> Livelink : </b> ${data.livelink}</p>
+        
+    </div>
+    `
+}
+
+// show modal
+const showmodal = (id) => {
+    // console.log(id, array)
+    if(id < 50){
+        for(let i=0; i<fullstack.length; i++){
+            if(fullstack[i].id == id){
+                // console.log(id)
+                const data = fullstack[i]
+                // console.log(data)
+                modalbody(data)
+    
+                break;
+            }
+        }
+    }
+    else if(id > 99){
+        for(let i=0; i<frontend.length; i++){
+            if(frontend[i].id == id){
+                // console.log(id)
+                const data = frontend[i]
+                // console.log(data)
+                modalbody(data)
+    
+                break;
+            }
+        }
+    }
+    else{
+        for(let i=0; i<backend.length; i++){
+            if(backend[i].id == id){
+                // console.log(id)
+                const data = backend[i]
+                // console.log(data)
+                modalbody(data)
+    
+                break;
+            }
+        }
+    }
+
+}
+
